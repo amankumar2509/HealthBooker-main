077750da7169a5521544b3565372c4bae25c9ebc
amankumar2509 <asr250901@gmail.com>
2023-07-19 17:20:58 +0530

    cors in server

--- server.js
+++ server.js
@@ -10,6 +10,20 @@ const notificationRouter = require("./routes/notificationRouter");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			// allow requests with no origin
+			// (like mobile apps or curl requests)
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		},
+	})
+);
 
 app.use(cors());
 app.use(express.json());
